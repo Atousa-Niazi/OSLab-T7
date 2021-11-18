e728767661a432c3ee7d1d2b72632beb680c3f14
Atousa Niazi <91827654+Atousa-Niazi@users.noreply.github.com>
2021-11-18 10:49:44 +0330

    Update main.py

--- python-fraction-class/main.py
+++ python-fraction-class/main.py
@@ -1,33 +1,65 @@
 #Atousa Niazi Abkoh-98440127-python-Fraction-class-OSLAb-T7_2
 # n for numerator / d for denominator
 
-class f:
-    def __init__(self, n1,d1,n2,d2):
-        self.f1 = n1/d1
-        self.f2 = n2/d2
-    
-    def Sum(self):
-        s=self.f1+self.f2
-        print('Sum of fractions: ', s)
-    def Sub(self):
-        u=self.f1 - self.f2
-        print('Sub of fractions: ',u)
-    def Mult(self):
-        m=self.f1*self.f2
-        print('Mult of fractions: ',m)
-    def Div(self):
-        d=self.f1 / self.f2
-        print('Div of fractions: ',d)
+class fraction:
+    def __init__(self, n=0,d=None):
+        self.n = n
+        self.d=d
+    def show(self):
+        print(self.n,"/",self.d)
+    def Sum(self,other):
+        numerator=self.n*other.d+self.d*other.n
+        denominator=self.d*other.d
+        result=fraction(numerator,denominator)
+        return result
+    def Sub(self,other):
+        numerator=self.n*other.d-self.d*other.n
+        denominator=self.d*other.d
+        result=fraction(numerator,denominator)
+        return result
+    def Mult(self,other):
+        numerator=self.n*other.n
+        denominator=self.d*other.d
+        result=fraction(numerator,denominator)
+        return result
+    def Div(self,other):
+        numerator=self.n*other.d
+        denominator=self.d*other.n
+        result=fraction(numerator,denominator)
+        return result
+
     
 print('welcome !')
 print('we need 4 ints for two fractions.')
 print('please enter 4 num:')
 n1=int(input())
 d1=int(input())
+while d1==0:
+    print('wrong num, try again:')
+    d1=int(input())
 n2=int(input())
 d2=int(input())
-p1 = f(n1,d1,n2,d2)
-p1.Sum()
-p1.Sub()
-p1.Mult()
-p1.Div()
+while d2==0:
+    print('wrong num, try again:')
+    d2=int(input())
+f1=fraction(n1,d1)
+f1.show()
+
+f2=fraction(n2,d2)
+f2.show()
+
+f3=f1.Sum(f2)
+print('Sum:')
+f3.show()
+
+f3=f1.Sub(f2)
+print('Sub:')
+f3.show()
+
+f3=f1.Mult(f2)
+print('Mult:')
+f3.show()
+
+f3=f1.Div(f2)
+print('Div:')
+f3.show()
